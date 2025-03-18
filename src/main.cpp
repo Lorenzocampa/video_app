c407497f31c41ced3de77da5221ef3f785c1d14e
campa <campanini.astor@gmail.com>
2025-03-18 17:42:36 +0100

    pixel data rendere

--- src/main.cpp
+++ src/main.cpp
@@ -36,19 +36,56 @@ int main() {
     }
   }
 
-  while (!glfwWindowShouldClose(window)) {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glDrawPixels(100, 100, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glfwSwapBuffers(window);
-
-    glfwPollEvents();
-
-    glfwSwapBuffers(window);
+  for (int y = 25; y < 75; ++y) {
+    for (int x = 25; x < 75; ++x) {
+      data[y * 100 * 3 + x * 3] = 0x00;
+      data[y * 100 * 3 + x * 3 + 1] = 0x00;
+      data[y * 100 * 3 + x * 3 + 2] = 0xff;
+    }
   }
 
-  glfwDestroyWindow(window);
-  glfwTerminate();
+  GLuint tex_handle;
+  glGenTextures(1, &tex_handle);
+  glBindTexture(GL_TEXTURE_2D, tex_handle);
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE,
+               data);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //*set up orphographic projection
+    int window_width, window_height;
+    glfwGetFramebufferSize(window, &window_width, &window_height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, window_width, 0, window_height, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+
+    //*rendere whatever you want
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, tex_handle);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex2f(100, 100);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex2f(window_width - 100, 100);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex2f(window_width - 100, window_height - 100);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex2f(100, window_height - 100);
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
 
   return 0;
 }
