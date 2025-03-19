d97e930130aed3fb2becfcf8fd8f9e811716a00a
campa <campanini.astor@gmail.com>
2025-03-19 12:02:50 +0100

    load first frame of mp4 video

---
+++ src/load_frame.cpp
@@ -0,0 +1,148 @@
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
+#include <fstream>
+
+bool load_frame(const char* filename, int* width_out, int* height_out, unsigned char** data_out)
+{
+	std::ifstream file(filename);
+	if (file.is_open())
+	{
+		printf("file found\n");
+		file.close();
+	}
+	else
+	{
+		printf("file not found\n");
+		return false;
+	}
+
+	//*open the file using libavformat
+	AVFormatContext* av_format_ctx = avformat_alloc_context();
+	if (!av_format_ctx)
+	{
+		fprintf(stderr, "could not allocate AVFormatContext\n");
+		return false;
+	}
+
+	if (avformat_open_input(&av_format_ctx, filename, NULL, NULL) != 0)
+	{
+		printf("attempt to open: %s\n", filename);
+		printf("couldn't open video file\n");
+		return false;
+	}
+
+	//*find the first valid video stream in the file
+	int video_stream_index = -1;
+	AVCodecParameters* av_codec_params;
+	const AVCodec* av_codec;
+
+	for (int i = 0; i < av_format_ctx->nb_streams; ++i)
+	{
+		av_codec_params = av_format_ctx->streams[i]->codecpar;
+		av_codec		= avcodec_find_decoder(av_codec_params->codec_id);
+
+		if (!av_codec)
+		{
+			continue;
+		}
+
+		if (av_codec_params->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			video_stream_index = i;
+			break;
+		}
+	}
+
+	if (video_stream_index == -1)
+	{
+		printf("couldn't find video stream\n");
+		return false;
+	}
+
+	//*set up a codec context for the decoder
+	AVCodecContext* av_code_ctx = avcodec_alloc_context3(av_codec);
+	if (!av_code_ctx)
+	{
+		printf("couldn't allocate AVCodecContext\n");
+		return false;
+	}
+	if (avcodec_parameters_to_context(av_code_ctx, av_codec_params) < 0)
+	{
+		printf("couldn't initialize AVCodecContext\n");
+		return false;
+	}
+	if (!avcodec_open2(av_code_ctx, av_codec, NULL) < 0)
+	{
+		printf("couldn't open codec\n");
+		return false;
+	}
+
+	AVFrame* av_frame = av_frame_alloc();
+	if (!av_frame)
+	{
+		printf("couldn't allocate AVFrame\n");
+		return false;
+	}
+	AVPacket* av_packet = av_packet_alloc();
+	if (!av_packet)
+	{
+		printf("couldn't allocate AVPacket\n");
+		return false;
+	}
+	int response;
+	while (av_read_frame(av_format_ctx, av_packet) >= 0)
+	{
+		if (av_packet->stream_index != video_stream_index)
+		{
+			continue;
+		}
+		response = avcodec_send_packet(av_code_ctx, av_packet);
+		if (response < 0)
+		{
+			printf("failed to decode packet: %s\n", av_err2str(response));
+			return false;
+		}
+		response = avcodec_receive_frame(av_code_ctx, av_frame);
+		if (response == AVERROR(EAGAIN) || response == AVERROR_EOF)
+		{
+			continue;
+		}
+		else if (response < 0)
+		{
+			printf("failed to decode packet: %s\n", av_err2str(response));
+			return false;
+		}
+		av_packet_unref(av_packet);
+		break;
+	}
+
+	uint8_t* data			   = new uint8_t[av_frame->width * av_frame->height * 4];
+	SwsContext* sws_scaler_ctx = sws_getContext(av_frame->width, av_frame->height, av_code_ctx->pix_fmt, av_frame->width, av_frame->height, AV_PIX_FMT_RGB0, SWS_BILINEAR, NULL, NULL, NULL);
+
+	if (!sws_scaler_ctx)
+	{
+		printf("couldn't initialize SwsContext\n");
+		return false;
+	}
+
+	uint8_t* dest[4]		 = {data, NULL, NULL, NULL};
+	int dest_linesize[4] = {av_frame->width * 4, 0, 0, 0};
+	sws_scale(sws_scaler_ctx, av_frame->data, av_frame->linesize, 0, av_frame->height, dest, dest_linesize);
+	sws_freeContext(sws_scaler_ctx);
+
+	*width_out  = av_frame->width;
+	*height_out = av_frame->height;
+	*data_out   = data;
+
+	avformat_close_input(&av_format_ctx);
+	avformat_free_context(av_format_ctx);
+	av_frame_free(&av_frame);
+	av_packet_free(&av_packet);
+	avcodec_free_context(&av_code_ctx);
+
+	return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,90 +2,85 @@
 #include <stdio.h>
 
 // Funzione di errore callback
-void error_callback(int error, const char *description) {
-  fprintf(stderr, "GLFW Error (%d): %s\n", error, description);
+void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "GLFW Error (%d): %s\n", error, description);
 }
 
-int main() {
-  // Imposta la callback per gli errori di GLFW
-  glfwSetErrorCallback(error_callback);
+bool load_frame(const char* filename, int* width, int* height, unsigned char** data);
 
-  // Inizializzazione di GLFW
-  if (!glfwInit()) {
-    printf("GLFW initialization failed!\n");
-    return 1;
-  }
+int main()
+{
+	glfwSetErrorCallback(error_callback);
 
-  GLFWwindow *window = glfwCreateWindow(640, 480, "OpenGL Window", NULL, NULL);
-  if (!window) {
-    printf("GLFW window creation failed!\n");
-    glfwTerminate();
-    return 1;
-  }
+	if (!glfwInit())
+	{
+		printf("GLFW initialization failed!\n");
+		return 1;
+	}
 
-  glfwMakeContextCurrent(window);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Window", NULL, NULL);
+	if (!window)
+	{
+		printf("GLFW window creation failed!\n");
+		glfwTerminate();
+		return 1;
+	}
 
-  glfwSwapInterval(1);
+	glfwMakeContextCurrent(window);
 
-  unsigned char *data = new unsigned char[100 * 100 * 3];
-  for (int y = 0; y < 100; ++y) {
-    for (int x = 0; x < 100; ++x) {
-      data[y * 100 * 3 + x * 3] = 0xff;
-      data[y * 100 * 3 + x * 3 + 1] = 0x00;
-      data[y * 100 * 3 + x * 3 + 2] = 0x00;
-    }
-  }
+	glfwSwapInterval(1);
 
-  for (int y = 25; y < 75; ++y) {
-    for (int x = 25; x < 75; ++x) {
-      data[y * 100 * 3 + x * 3] = 0x00;
-      data[y * 100 * 3 + x * 3 + 1] = 0x00;
-      data[y * 100 * 3 + x * 3 + 2] = 0xff;
-    }
-  }
+	int frame_width, frame_height;
+	unsigned char* frame_data;
+	if (!load_frame("/mnt/c/Users/astor_dev_03/Desktop/video.mkv", &frame_width, &frame_height, &frame_data))
+	{
+		printf("could not load frame!\n");
+		return 1;
+	};
 
-  GLuint tex_handle;
-  glGenTextures(1, &tex_handle);
-  glBindTexture(GL_TEXTURE_2D, tex_handle);
-  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE,
-               data);
+	GLuint tex_handle;
+	glGenTextures(1, &tex_handle);
+	glBindTexture(GL_TEXTURE_2D, tex_handle);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, frame_data);
 
-  while (!glfwWindowShouldClose(window)) {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //*set up orphographic projection
-    int window_width, window_height;
-    glfwGetFramebufferSize(window, &window_width, &window_height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, window_width, 0, window_height, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
+		//*set up orphographic projection
+		int window_width, window_height;
+		glfwGetFramebufferSize(window, &window_width, &window_height);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, window_width, window_height, 0, -1, 1);
+		glMatrixMode(GL_MODELVIEW);
 
-    //*rendere whatever you want
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, tex_handle);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 0.0f);
-    glVertex2f(100, 100);
-    glTexCoord2f(1.0f, 0.0f);
-    glVertex2f(window_width - 100, 100);
-    glTexCoord2f(1.0f, 1.0f);
-    glVertex2f(window_width - 100, window_height - 100);
-    glTexCoord2f(0.0f, 1.0f);
-    glVertex2f(100, window_height - 100);
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+		//*rendere whatever you want
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, tex_handle);
+		glBegin(GL_QUADS);
+		glTexCoord2f(0.0f, 0.0f);
+		glVertex2f(200, 200);
+		glTexCoord2f(1.0f, 0.0f);
+		glVertex2f(200 + frame_width, 200);
+		glTexCoord2f(1.0f, 1.0f);
+		glVertex2f(200 + frame_width, 200 + frame_height);
+		glTexCoord2f(0.0f, 1.0f);
+		glVertex2f(200, 200 + frame_height);
+		glEnd();
+		glDisable(GL_TEXTURE_2D);
 
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
-  return 0;
+	return 0;
 }
